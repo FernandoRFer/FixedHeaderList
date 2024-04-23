018799d6ffeb57f758092fc3b69daeb749791614
Fernando Fernandes <fernandes@itstransdata.com>
2024-04-23 14:36:21 -0300

    teste de package

--- lib/header_fixed.dart
+++ lib/header_fixed.dart
@@ -1,12 +1,25 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:signals/signals_flutter.dart';
 
+class ListHeader {
+  Widget header;
+  Size size;
+  ListHeader({
+    this.header = const SizedBox(),
+    this.size = const Size(0, 0),
+  });
+}
+
 class HeaderFixed extends StatefulWidget {
-  const HeaderFixed({super.key, required this.title});
-
-  final String title;
-
+  final List<List<Widget>> listItens;
+  const HeaderFixed({
+    super.key,
+    required this.listItens,
+  });
   @override
   State<HeaderFixed> createState() => _HeaderFixedState();
 }
@@ -14,8 +27,6 @@ class HeaderFixed extends StatefulWidget {
 class _HeaderFixedState extends State<HeaderFixed> {
   double? heightAppBar;
   double? heightPaddingTop;
-  final visibleHeader1 = signal(false);
-  final visibleHeader2 = signal(false);
   Size header1Size = Size.zero;
   Size header2Size = Size.zero;
   List<Size> listSize = [];
@@ -26,53 +37,41 @@ class _HeaderFixedState extends State<HeaderFixed> {
   String title1 = "Header - 1";
   String title2 = "Header - 2";
 
-  List<List<Widget>> listItens = [
-    testList(),
-    testList2(),
-    testList3(),
-    testList(),
-    testList2(),
-    testList3(),
-    testList(),
-    testList2(),
-    testList3()
-  ];
-
   List<List<Widget>> listItensKey = [];
 
-  List<Widget> listHeader = [];
-  Widget? currentHeader;
-
-  final listVisible = signal([]);
+  List<ListHeader> listHeader = [];
+  ListHeader currentHeader = ListHeader();
+  List<bool> listVisible = [];
 
   late List<GlobalKey> _keys;
 
   List<RenderBox> listRender = [];
 
+  bool isFirstconfiguration = true;
+
   @override
   void initState() {
     super.initState();
-
-    log("initState");
     _keys = List<GlobalKey>.generate(
-      listItens.length,
+      widget.listItens.length,
       (int i) => GlobalKey(),
     );
 
     _scrollController.addListener(_updatePosition);
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      // _insertKeys();
+    });
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-
     _insertKeys();
   }
 
   @override
   void dispose() {
     super.dispose();
-
     _scrollController.dispose();
   }
   // void _scrollListener() {
@@ -90,32 +89,37 @@ class _HeaderFixedState extends State<HeaderFixed> {
 
   void _insertKeys() {
     log("_insertKeys");
-    for (var i = 0; listItens.length > i; i++) {
-      Widget header = Container(key: _keys[i], child: listItens[i][0]);
-      listHeader.add(Container(
-          alignment: Alignment.topCenter,
-          height: _keys[i].currentContext?.size?.height ?? 0,
-          child: listItens[i][0]));
-      listItens[i].removeAt(0);
-      i == 0 ? listVisible.value.add(true) : listVisible.value.add(false);
-      listItensKey.add([header, ...listItens[i]]);
+    for (var i = 0; widget.listItens.length > i; i++) {
+      Widget header = Container(key: _keys[i], child: widget.listItens[i][0]);
+
+      listHeader.add(ListHeader(
+          header: Container(
+              alignment: Alignment.topCenter,
+              height: _keys[i].currentContext?.size?.height ?? 0,
+              child: widget.listItens[i][0]),
+          size: _keys[i].currentContext?.size ?? const Size(0, 0)));
+
+      widget.listItens[i].removeAt(0);
+
+      i == 0 ? listVisible.add(true) : listVisible.add(false);
+
+      listItensKey.add([header, ...widget.listItens[i]]);
     }
   }
 
   void _visible(int? positionList) {
     for (var indexVisible = 0;
-        listVisible.value.length > indexVisible;
+        listVisible.length > indexVisible;
         indexVisible++) {
-      listVisible.value[indexVisible] = false;
+      listVisible[indexVisible] = false;
     }
 
     if (positionList != null) {
       currentHeader = listHeader[positionList];
-      listVisible.value[positionList] = true;
+      listVisible[positionList] = true;
     } else {
-      currentHeader = const SizedBox();
+      currentHeader = ListHeader();
     }
-
     setState(() {});
   }
 
@@ -131,68 +135,35 @@ class _HeaderFixedState extends State<HeaderFixed> {
     }
 
     for (int i = 0; positions.length > i; i++) {
-      if (!(listVisible.value[i])) {
-        if (positions.length > (i + 1) &&
-            positions[i] <= 0 &&
-            positions[i + 1] >= 0 &&
-            positions[i + 1] - 150.0 >= 0) {
-          log(positions[i].toString());
-          _visible(i);
-          break;
+      if (!(listVisible[i])) {
+        if (positions.length == i + 1) {
+          if ((positions[i]) <= 0) {
+            _visible(i);
+            break;
+          }
+        } else {
+          if (positions[i] <= 0 &&
+              positions[i + 1] >= 0 &&
+              positions[i + 1] - 150.0 >= 0) {
+            log(positions[i].toString());
+            _visible(i);
+            break;
+          }
         }
-        // if (positions.length == (i + 1) && positions[i] <= 0) {
-        //   _visible(i);
-        //   break;
-        // }
       } else {
-        if (positions[i] <= 0 && (positions[i + 1] - 150.0) <= 0) {
-          log("null --- ${positions[i]}");
-          _visible(null);
-          break;
+        if (positions.length == i + 1) {
+          if ((positions[i]) >= 0) {
+            _visible(null);
+            break;
+          }
+        } else {
+          if (positions[i] <= 0 && (positions[i + 1] - 150.0) <= 0) {
+            _visible(null);
+            break;
+          }
         }
       }
     }
-    // Melhorar incialização de metodo
-    // final RenderBox header1 =
-    //     keyHeader1.currentContext!.findRenderObject() as RenderBox;
-    // final RenderBox header2 =
-    //     keyHeader2.currentContext!.findRenderObject() as RenderBox;
-
-    // if (header1Size == Size.zero && header2Size == Size.zero) {
-    //   header1Size = header1.size;
-    //   header2Size = header2.size;
-    // }
-
-    // final renderBoxHeadPrimari = header1.localToGlobal(Offset.zero).dy -
-    //     (heightAppBar ?? 0.0) -
-    //     (heightPaddingTop ?? 0.0);
-
-    // final renderBoxRedLast = header2.localToGlobal(Offset.zero).dy -
-    //     (heightAppBar ?? 0.0) -
-    //     (heightPaddingTop ?? 0.0);
-
-    // header1Size = header1.size;
-    // header2Size = header2.size;
-
-    // if (renderBoxHeadPrimari <= 0 && (renderBoxRedLast - 24) >= 0) {
-    //   if (visibleHeader1.value != true) {
-    //     visibleHeader1.value = true;
-    //   }
-    // } else {
-    //   if (visibleHeader1.value != false) {
-    //     visibleHeader1.value = false;
-    //   }
-    // }
-
-    // if (renderBoxRedLast <= 0) {
-    //   if (visibleHeader2.value != true) {
-    //     visibleHeader2.value = true;
-    //   }
-    // } else {
-    //   if (visibleHeader2.value != false) {
-    //     visibleHeader2.value = false;
-    //   }
-    // }
   }
 
   AppBar _appBar() {
@@ -202,36 +173,18 @@ class _HeaderFixedState extends State<HeaderFixed> {
     );
   }
 
-  Widget _header({GlobalKey? globalKey, required String title}) {
-    return Padding(
-      key: globalKey,
-      padding: const EdgeInsets.only(top: 8, bottom: 8),
-      child: Text(title,
-          style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 24)),
-    );
-  }
-
-  Widget _headerFixed(String title) {
-    return Container(
-        alignment: Alignment.topCenter,
-        height: header1Size.height,
-        color: Colors.white,
-        child: _header(title: title));
-  }
-
-  bool _configuration(BuildContext context) {
-    currentHeader ??= listHeader[0];
+  void _configuration(BuildContext context) {
     if (heightAppBar == null || heightPaddingTop == null) {
       heightAppBar = _appBar().preferredSize.height;
       heightPaddingTop = MediaQuery.of(context).padding.top;
     }
-    return true;
+
+    isFirstconfiguration = false;
   }
 
   @override
   Widget build(BuildContext context) {
-    // _insertKeys();
-    if (heightPaddingTop == null) {
+    if (isFirstconfiguration) {
       _configuration(context);
     }
 
@@ -250,63 +203,9 @@ class _HeaderFixedState extends State<HeaderFixed> {
                   ),
                 ],
               )),
-          SizedBox(height: 150, child: currentHeader!),
-
-          // ...listHeader.map((e) {log(e.key.toString());})
+          SizedBox(height: 150, child: currentHeader.header),
         ],
       ),
     );
   }
-}
-
-List<Widget> testList() {
-  return [
-    Container(
-        alignment: Alignment.center,
-        height: 150,
-        color: const Color.fromARGB(255, 255, 255, 255),
-        child: const Text("Header 1")),
-    Container(height: 150, color: Colors.yellowAccent),
-    Container(height: 150, color: Colors.yellow),
-    Container(height: 150, color: Colors.yellowAccent),
-    Container(height: 150, color: Colors.yellow),
-    Container(height: 150, color: Colors.yellowAccent),
-    Container(height: 150, color: Colors.yellow),
-  ];
-}
-
-List<Widget> testList2() {
-  return [
-    Container(
-        alignment: Alignment.center,
-        height: 150,
-        color: Colors.red,
-        child: const Text("Header 1")),
-    Container(height: 150, color: Colors.yellowAccent),
-    Container(height: 150, color: Colors.yellow),
-    Container(height: 150, color: Colors.yellowAccent),
-    Container(height: 150, color: Colors.yellow),
-    Container(height: 150, color: Colors.yellowAccent),
-    Container(height: 150, color: Colors.yellow),
-  ];
-}
-
-List<Widget> testList3() {
-  return [
-    Container(
-        alignment: Alignment.center,
-        height: 150,
-        color: Colors.green,
-        child: const Center(
-          child: Text(
-            "Header 3",
-          ),
-        )),
-    Container(height: 150, color: Colors.yellowAccent),
-    Container(height: 150, color: Colors.yellow),
-    Container(height: 150, color: Colors.yellowAccent),
-    Container(height: 150, color: Colors.yellow),
-    Container(height: 150, color: Colors.yellowAccent),
-    Container(height: 150, color: Colors.yellow),
-  ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:signals/signals_flutter.dart';
+import 'package:header/header_fixed.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,169 +16,71 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  double? heightAppBar;
-  double? heightPaddingTop;
-  final visibleHeader1 = signal(false);
-  final visibleHeader2 = signal(false);
-  Size header1Size = Size.zero;
-  Size header2Size = Size.zero;
-
-  GlobalKey keyHeader1 = GlobalKey();
-  GlobalKey keyHeader2 = GlobalKey();
-  final ScrollController _scrollController = ScrollController();
-
-  String title1 = "Header - 1";
-  String title2 = "Header - 2";
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController.addListener(_updatePosition);
-  }
-
-  // void _scrollListener() {
-  //   double posicaoInicial = 100; // Substitua com a posição inicial desejada
-  //   if (_scrollController.offset >= posicaoInicial && _position != 1.0) {
-  //     setState(() {
-  //       _position = _scrollController.offset;
-  //     });
-  //   } else if (_scrollController.offset < posicaoInicial && _position != 0.0) {
-  //     setState(() {
-  //       _position = 0.0;
-  //     });
-  //   }
-  // }
-
-  void _updatePosition() {
-    // Melhorar incialização de metodo
-    final RenderBox header1 =
-        keyHeader1.currentContext!.findRenderObject() as RenderBox;
-    final RenderBox header2 =
-        keyHeader2.currentContext!.findRenderObject() as RenderBox;
-
-    if (header1Size == Size.zero && header2Size == Size.zero) {
-      header1Size = header1.size;
-      header2Size = header2.size;
-    }
-
-    final renderBoxHeadPrimari = header1.localToGlobal(Offset.zero).dy -
-        (heightAppBar ?? 0.0) -
-        (heightPaddingTop ?? 0.0);
-
-    final renderBoxRedLast = header2.localToGlobal(Offset.zero).dy -
-        (heightAppBar ?? 0.0) -
-        (heightPaddingTop ?? 0.0);
-
-    header1Size = header1.size;
-    header2Size = header2.size;
-
-    if (renderBoxHeadPrimari <= 0 && (renderBoxRedLast - 24) >= 0) {
-      if (visibleHeader1.value != true) {
-        visibleHeader1.value = true;
-      }
-    } else {
-      if (visibleHeader1.value != false) {
-        visibleHeader1.value = false;
-      }
-    }
-
-    if (renderBoxRedLast <= 0) {
-      if (visibleHeader2.value != true) {
-        visibleHeader2.value = true;
-      }
-    } else {
-      if (visibleHeader2.value != false) {
-        visibleHeader2.value = false;
-      }
-    }
-  }
-
-  AppBar _appBar() {
-    return AppBar(
-      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      title: const Text("Fixed headers"),
-    );
-  }
-
-  Widget _header({GlobalKey? globalKey, required String title}) {
-    return Padding(
-      key: globalKey,
-      padding: const EdgeInsets.only(top: 8, bottom: 8),
-      child: Text(title,
-          style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 24)),
-    );
-  }
-
-  Widget _headerFixed(String title) {
-    return Container(
-        alignment: Alignment.topCenter,
-        height: header1Size.height,
-        color: Colors.white,
-        child: _header(title: title));
-  }
-
-  bool _configuration(BuildContext context) {
-    if (heightAppBar == null || heightPaddingTop == null) {
-      heightAppBar = _appBar().preferredSize.height;
-      heightPaddingTop = MediaQuery.of(context).padding.top;
-    }
-    return true;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    _configuration(context);
-
-    return Scaffold(
-      appBar: _appBar(),
-      body: Stack(
-        children: [
-          SingleChildScrollView(
-            controller: _scrollController,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                _header(globalKey: keyHeader1, title: title1),
-                Container(height: 150, color: Colors.yellow),
-                Container(height: 150, color: Colors.yellowAccent),
-                Container(height: 150, color: Colors.yellow),
-                Container(height: 150, color: Colors.yellowAccent),
-                Container(height: 150, color: Colors.yellow),
-                _header(globalKey: keyHeader2, title: title2),
-                Container(height: 150, color: Colors.grey),
-                Container(height: 150, color: Colors.grey[400]),
-                Container(height: 150, color: Colors.grey),
-                Container(height: 150, color: Colors.grey[400]),
-                Container(height: 150, color: Colors.grey),
-                Container(height: 150, color: Colors.grey[400]),
-              ],
-            ),
-          ),
-          Visibility(
-            visible: visibleHeader1.watch(context),
-            child: _headerFixed(title1),
-          ),
-          Visibility(
-            visible: visibleHeader2.watch(context),
-            child: _headerFixed(title2),
-          ),
+      home: HeaderFixed(
+        listItens: [
+          testList(),
+          testList2(),
+          testList3(),
+          testList(),
+          testList2(),
+          testList3(),
+          testList(),
+          testList2(),
+          testList3()
         ],
       ),
     );
   }
+}
+
+List<Widget> testList() {
+  return [
+    Container(
+        alignment: Alignment.center,
+        height: 150,
+        color: const Color.fromARGB(255, 255, 255, 255),
+        child: const Text("Header 1")),
+    Container(height: 150, color: Colors.yellowAccent),
+    Container(height: 150, color: Colors.yellow),
+    Container(height: 150, color: Colors.yellowAccent),
+    Container(height: 150, color: Colors.yellow),
+    Container(height: 150, color: Colors.yellowAccent),
+    Container(height: 150, color: Colors.yellow),
+  ];
+}
+
+List<Widget> testList2() {
+  return [
+    Container(
+        alignment: Alignment.center,
+        height: 150,
+        color: Colors.red,
+        child: const Text("Header 1")),
+    Container(height: 150, color: Colors.yellowAccent),
+    Container(height: 150, color: Colors.yellow),
+    Container(height: 150, color: Colors.yellowAccent),
+    Container(height: 150, color: Colors.yellow),
+    Container(height: 150, color: Colors.yellowAccent),
+    Container(height: 150, color: Colors.yellow),
+  ];
+}
+
+List<Widget> testList3() {
+  return [
+    Container(
+        alignment: Alignment.center,
+        height: 150,
+        color: Colors.green,
+        child: const Center(
+          child: Text(
+            "Header 3",
+          ),
+        )),
+    Container(height: 150, color: Colors.yellowAccent),
+    Container(height: 150, color: Colors.yellow),
+    Container(height: 150, color: Colors.yellowAccent),
+    Container(height: 150, color: Colors.yellow),
+    Container(height: 150, color: Colors.yellowAccent),
+    Container(height: 150, color: Colors.yellow),
+  ];
 }
